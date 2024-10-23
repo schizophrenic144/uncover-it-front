9265f5f0e54cdbf9b98dbb87a151ae25184839a5
WarFiN <rajvirgoenka@gmail.com>
2024-10-23 15:21:19 +0530

    Add site MetaData

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,54 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+export const metadata: Metadata = {
+  title: {
+    default: "Uncover it",
+    template: "Uncover it| %s",
+  },
+  description:
+    "Uncover the hidden malware, don't let it uncover you!",
+  openGraph: {
+    title: "Uncover it",
+    description:
+      "Uncover the hidden malware, don't let it uncover you!",
+    url: "https://uncover.us.kg",
+    siteName: "uncover.us.kg",
+    images: [
+      {
+        url: "https://github.com/WarFiN123/docs-bugattipvp/blob/main/app/opengraph-image.png?raw=true",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large" as const,
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: "Uncover it",
+    card: "summary_large_image",
+    description:
+      "Uncover the hidden malware, don't let it uncover you!",
+    images: [
+      "https://github.com/WarFiN123/docs-bugattipvp/blob/main/app/opengraph-image.png?raw=true",
+    ],
+  },
+  icons: {
+    shortcut: "/favicon.ico",
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
